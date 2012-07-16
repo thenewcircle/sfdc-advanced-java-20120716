ac28c6d66e3faaa6cdbed3a60885e24e984d4c8f
Dan Rosen <dan@marakana.com>
2012-07-16 15:02:52 -0700

    going back to Cons and Nil ...

--- src/immutablelist/ImmutableList.java
+++ src/immutablelist/ImmutableList.java
@@ -1,35 +1,11 @@
 package immutablelist;
 
-import java.util.ArrayList;
-import java.util.List;
 
-public class ImmutableList<T> {
+public interface ImmutableList<T> {
 
-	private final List<T> list;
-
-	public ImmutableList() {
-		this.list = new ArrayList<T>();
-	}
-
-	private ImmutableList(List<T> list) {
-		this.list = list;
-	}
-
-	public ImmutableList<T> prepend(T element) {
-		List<T> copy = new ArrayList<T>();
-		copy.add(element);
-		copy.addAll(list);
-		return new ImmutableList<T>(copy);
-	}
-
-	public T head() { return list.get(0); }
-
-	public ImmutableList<T> tail() {
-		List<T> copy = new ArrayList<T>(list);
-		copy.remove(0);
-		return new ImmutableList<T>(copy);
-	}
-
-	public boolean isEmpty() { return list.isEmpty(); }
+	ImmutableList<T> prepend(T element);
+	T head();
+	ImmutableList<T> tail();
+	boolean isEmpty();
 
 }
