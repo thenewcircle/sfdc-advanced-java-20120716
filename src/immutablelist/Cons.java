ec001f0ff66fd73c23c0383757a491d09d999880
Dan Rosen <dan@marakana.com>
2012-07-16 15:13:29 -0700

    implemented Cons and Nil, without tests or anything interesting

--- src/immutablelist/Cons.java
+++ src/immutablelist/Cons.java
@@ -2,4 +2,32 @@ package immutablelist;
 
 public class Cons<T> implements ImmutableList<T> {
 
+	private final T head;
+	private final ImmutableList<T> tail;
+
+	public Cons(T head, ImmutableList<T> tail) {
+		this.head = head;
+		this.tail = tail;
+	}
+
+	@Override
+	public ImmutableList<T> prepend(T element) {
+		return new Cons<T>(element, this);
+	}
+
+	@Override
+	public T head() {
+		return head;
+	}
+
+	@Override
+	public ImmutableList<T> tail() {
+		return tail;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+
 }
--- src/immutablelist/Nil.java
+++ src/immutablelist/Nil.java
@@ -1,5 +1,27 @@
 package immutablelist;
 
+import java.util.NoSuchElementException;
+
 public class Nil<T> implements ImmutableList<T> {
+
+	@Override
+	public ImmutableList<T> prepend(T element) {
+		return new Cons<T>(element, this);
+	}
+
+	@Override
+	public T head() {
+		throw new NoSuchElementException();
+	}
+
+	@Override
+	public ImmutableList<T> tail() {
+		throw new IndexOutOfBoundsException();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return true;
+	}
 
 }
