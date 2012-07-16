cffce64852f7becfda275f70107becec7c280298
Dan Rosen <dan@marakana.com>
2012-07-16 14:46:17 -0700

    implemented ImmutableList using underlying List

--- src/immutablelist/ImmutableList.java
+++ src/immutablelist/ImmutableList.java
@@ -5,13 +5,31 @@ import java.util.List;
 
 public class ImmutableList<T> {
 
-	private final List<T> list = new ArrayList<T>();
+	private final List<T> list;
 
-	public ImmutableList() {}
-	public ImmutableList<T> prepend(T element) { return null; }
+	public ImmutableList() {
+		this.list = new ArrayList<T>();
+	}
 
-	public T head() { return null; }
-	public ImmutableList<T> tail() { return null; }
-	public boolean isEmpty() { return false; }
+	private ImmutableList(List<T> list) {
+		this.list = list;
+	}
+
+	public ImmutableList<T> prepend(T element) {
+		List<T> copy = new ArrayList<T>();
+		copy.add(element);
+		copy.addAll(list);
+		return new ImmutableList<T>(copy);
+	}
+
+	public T head() { return list.get(0); }
+
+	public ImmutableList<T> tail() {
+		List<T> copy = new ArrayList<T>(list);
+		copy.remove(0);
+		return new ImmutableList<T>(copy);
+	}
+
+	public boolean isEmpty() { return list.isEmpty(); }
 
 }
