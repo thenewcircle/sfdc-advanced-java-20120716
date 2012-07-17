0a56e70c6c8867dca1e693e3e845bc56a23926fb
Dan Rosen <dan@marakana.com>
2012-07-17 16:59:59 -0700

    implemented producer/consumer using BlockingQueue

--- src/logger/Main.java
+++ src/logger/Main.java
@@ -1,31 +1,22 @@
 package logger;
 
-import immutablelist.ImmutableList;
-import immutablelist.Nil;
-
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		final AtomicReference<ImmutableList<Integer>> work =
-				new AtomicReference<ImmutableList<Integer>>(new Nil<Integer>());
+		final BlockingQueue<Integer> work = new LinkedBlockingQueue<Integer>(1000);
 
 		for (int i = 0; i < 10; i++) {
 			Thread t = new Thread() {
 				public void run() {
 					while (true) {
 						try {
-							Thread.sleep(100);
+							work.put(new Random().nextInt(30));
 						} catch (InterruptedException e) {}
-
-						ImmutableList<Integer> prev = null, next = null;
-						do {
-							prev = work.get();
-							next = prev.prepend(new Random().nextInt(30));
-						} while (!work.compareAndSet(prev, next));
 					}
 				}
 			};
@@ -35,11 +26,9 @@ public class Main {
 		Thread t = new Thread() {
 			public void run() {
 				while (true) {
-					ImmutableList<Integer> prev = null;
-					do {
-						prev = work.get();
-					} while (prev.isEmpty() || !work.compareAndSet(prev, prev.tail()));
-					System.out.println(Fibonacci.fib(prev.head()));
+					try {
+						System.out.println(Fibonacci.fib(work.take()));
+					} catch (InterruptedException e) {}
 				}
 			}
 		};
