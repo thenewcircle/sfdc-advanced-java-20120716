45d86764658860c1438d82a23233a7c2757cb9e5
Dan Rosen <dan@marakana.com>
2012-07-17 16:02:13 -0700

    setup for AtomicReference<ImmutableList<Integer>>

--- src/logger/Main.java
+++ src/logger/Main.java
@@ -1,14 +1,18 @@
 package logger;
 
+import immutablelist.ImmutableList;
+import immutablelist.Nil;
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Main {
 
 	public static void main(String[] args) {
-		final Queue<Integer> work = new LinkedList<Integer>();
-		final Object monitor = new Object();
+		final AtomicReference<ImmutableList<Integer>> work =
+				new AtomicReference<ImmutableList<Integer>>(new Nil<Integer>());
 
 		for (int i = 0; i < 10; i++) {
 			Thread t = new Thread() {
