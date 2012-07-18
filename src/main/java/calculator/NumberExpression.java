fc1c04299c141878c0329f88b22d0b2ffb9104f1
Dan Rosen <dan@marakana.com>
2012-07-18 13:48:27 -0700

    implemented Expression hierarchy

--- src/main/java/calculator/NumberExpression.java
+++ src/main/java/calculator/NumberExpression.java
@@ -1,11 +1,47 @@
 package calculator;
 
-public class NumberExpression implements Expression {
+public final class NumberExpression implements Expression {
+
+	private final int number;
+
+	public NumberExpression(int number) {
+		this.number = number;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + number;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NumberExpression other = (NumberExpression) obj;
+		if (number != other.number)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return String.valueOf(number);
+	}
 
 	@Override
 	public int value() {
-		// TODO Auto-generated method stub
-		return 0;
+		return number;
 	}
 
 }
--- src/main/java/calculator/OperationExpression.java
+++ src/main/java/calculator/OperationExpression.java
@@ -1,11 +1,36 @@
 package calculator;
 
-public class OperationExpression implements Expression {
+public final class OperationExpression implements Expression {
+
+	private final Operator op;
+	private final Expression lhs, rhs;
+
+	public OperationExpression(Operator op, Expression lhs, Expression rhs) {
+		this.op = op;
+		this.lhs = lhs;
+		this.rhs = rhs;
+	}
+
+	public Operator getOp() {
+		return op;
+	}
+
+	public Expression getLhs() {
+		return lhs;
+	}
+
+	public Expression getRhs() {
+		return rhs;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + lhs + op + rhs + ")";
+	}
 
 	@Override
 	public int value() {
-		// TODO Auto-generated method stub
-		return 0;
+		return op.apply(lhs.value(), rhs.value());
 	}
 
 }
