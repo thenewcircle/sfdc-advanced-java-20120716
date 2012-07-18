1f38ade75c88ddedeabba681b94ce37ea1c6e219
Dan Rosen <dan@marakana.com>
2012-07-18 10:30:29 -0700

    added calculator stub

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,22 @@
+package calculator;
+
+import java.util.Stack;
+
+public class Calculator {
+
+	public static boolean parseNumber(String token, Stack<Integer> stack) {
+		return false; // TODO
+	}
+
+	public static boolean parseOperator(String token, Stack<Integer> stack) {
+		return false; // TODO
+	}
+
+	public static int evaluate(String expression) {
+		Stack<Integer> stack = new Stack<Integer>();
+		String[] tokens = expression.split(" ");
+
+		return 0; // TODO
+	}
+
+}
