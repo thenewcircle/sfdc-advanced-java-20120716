7c9e60d06b9ea6d737ed25886b35f68ccce061a3
Dan Rosen <dan@marakana.com>
2012-07-18 13:27:21 -0700

    added stubs for Expression hierarchy

---
+++ src/main/java/calculator/NumberExpression.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class NumberExpression implements Expression {
+
+	@Override
+	public int value() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/main/java/calculator/OperationExpression.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class OperationExpression implements Expression {
+
+	@Override
+	public int value() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
